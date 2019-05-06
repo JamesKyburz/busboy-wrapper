c71c7e228d92a639161f1563e25566d0a1456aac
James Kyburz <james.kyburz@gmail.com>
2019-05-06 15:31:04 +0200

    tests replace tape with tap

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-const test = require('tape')
+const { test } = require('tap')
 const FormData = require('form-data')
 const fs = require('fs')
 const concat = require('concat-stream')
@@ -27,27 +27,33 @@ test('upload license file', async t => {
 
   const url = await listen(fn)
 
-  form.submit(url, (err, res) => {
-    t.error(err, 'server returned a reponse')
-    t.equals(res.statusCode, 200, 'server response is 200')
-    res.pipe(
-      concat(result => {
-        const json = JSON.parse(result)
-        t.equals(Object.keys(json.fields).length, 1, 'one field in response')
-        t.equals(json.fields.field, 'test', 'field = test')
-        t.equals(Object.keys(json.files).length, 1, 'one file in response')
-        t.equals(json.files.license.name, 'LICENSE', 'license name is correct')
-        t.ok(json.files.license.path, 'path is set')
-        t.equals(
-          json.files.license.hash,
-          '4f54dcf7b324c315b2cc2831b6b442515c7437c2',
-          'license hash is correct'
-        )
-        t.equals(json.files.license.size, 1816, 'license size is correct')
-        t.end()
-      })
-    )
-    res.resume()
+  await new Promise((resolve, reject) => {
+    form.submit(url, (err, res) => {
+      if (err) return reject(err)
+      t.equals(res.statusCode, 200, 'server response is 200')
+      res.pipe(
+        concat(result => {
+          const json = JSON.parse(result)
+          t.equals(Object.keys(json.fields).length, 1, 'one field in response')
+          t.equals(json.fields.field, 'test', 'field = test')
+          t.equals(Object.keys(json.files).length, 1, 'one file in response')
+          t.equals(
+            json.files.license.name,
+            'LICENSE',
+            'license name is correct'
+          )
+          t.ok(json.files.license.path, 'path is set')
+          t.equals(
+            json.files.license.hash,
+            '4f54dcf7b324c315b2cc2831b6b442515c7437c2',
+            'license hash is correct'
+          )
+          t.equals(json.files.license.size, 1816, 'license size is correct')
+          resolve()
+        })
+      )
+      res.resume()
+    })
   })
 })
 
@@ -58,20 +64,22 @@ test('multipart with fields and no file', async t => {
 
   const url = await listen(fn)
 
-  form.submit(url, (err, res) => {
-    t.error(err, 'server returned a reponse')
-    t.equals(res.statusCode, 200, 'server response is 200')
-    res.pipe(
-      concat(result => {
-        const json = JSON.parse(result)
-        t.equals(Object.keys(json.fields).length, 2, 'two fields in response')
-        t.equals(json.fields.field1, 'test1', 'field1 = test1')
-        t.equals(json.fields.field2, 'test2', 'field2 = test2')
-        t.equals(Object.keys(json.files).length, 0, 'no files in response')
-        t.end()
-      })
-    )
-    res.resume()
+  await new Promise((resolve, reject) => {
+    form.submit(url, (err, res) => {
+      if (err) return reject(err)
+      t.equals(res.statusCode, 200, 'server response is 200')
+      res.pipe(
+        concat(result => {
+          const json = JSON.parse(result)
+          t.equals(Object.keys(json.fields).length, 2, 'two fields in response')
+          t.equals(json.fields.field1, 'test1', 'field1 = test1')
+          t.equals(json.fields.field2, 'test2', 'field2 = test2')
+          t.equals(Object.keys(json.files).length, 0, 'no files in response')
+          resolve()
+        })
+      )
+      res.resume()
+    })
   })
 })
 
@@ -82,42 +90,48 @@ test('upload 2 files', async t => {
 
   const url = await listen(fn)
 
-  form.submit(url, (err, res) => {
-    t.error(err, 'server returned a reponse')
-    t.equals(res.statusCode, 200, 'server response is 200')
-    res.pipe(
-      concat(result => {
-        const json = JSON.parse(result)
-        t.equals(Object.keys(json.fields).length, 0, 'no fields in response')
-        t.equals(Object.keys(json.files).length, 2, 'two files in response')
-        t.ok(json.files.license.path, 'license path is set')
-        t.equals(json.files.license.name, 'LICENSE', 'license name is correct')
-        t.equals(
-          json.files.license.hash,
-          '4f54dcf7b324c315b2cc2831b6b442515c7437c2',
-          'license hash is correct'
-        )
-        t.equals(json.files.license.size, 1816, 'license size is correct')
-        t.equals(
-          json.files['.gitignore'].name,
-          '.gitignore',
-          '.gitignore name is correct'
-        )
-        t.ok(json.files['.gitignore'].path, '.gitignore path is set')
-        t.equals(
-          json.files['.gitignore'].hash,
-          'b026f212ac92d67fd5cdcce2ef34f1a3b15c3de5',
-          '.gitignore hash is correct'
-        )
-        t.equals(
-          json.files['.gitignore'].size,
-          37,
-          '.gitignore size is correct'
-        )
-        t.end()
-      })
-    )
-    res.resume()
+  await new Promise((resolve, reject) => {
+    form.submit(url, (err, res) => {
+      if (err) return reject(err)
+      t.equals(res.statusCode, 200, 'server response is 200')
+      res.pipe(
+        concat(result => {
+          const json = JSON.parse(result)
+          t.equals(Object.keys(json.fields).length, 0, 'no fields in response')
+          t.equals(Object.keys(json.files).length, 2, 'two files in response')
+          t.ok(json.files.license.path, 'license path is set')
+          t.equals(
+            json.files.license.name,
+            'LICENSE',
+            'license name is correct'
+          )
+          t.equals(
+            json.files.license.hash,
+            '4f54dcf7b324c315b2cc2831b6b442515c7437c2',
+            'license hash is correct'
+          )
+          t.equals(json.files.license.size, 1816, 'license size is correct')
+          t.equals(
+            json.files['.gitignore'].name,
+            '.gitignore',
+            '.gitignore name is correct'
+          )
+          t.ok(json.files['.gitignore'].path, '.gitignore path is set')
+          t.equals(
+            json.files['.gitignore'].hash,
+            '5deb8ce2d089fb11d9be6ab1e839f3a719a55915',
+            '.gitignore hash is correct'
+          )
+          t.equals(
+            json.files['.gitignore'].size,
+            49,
+            '.gitignore size is correct'
+          )
+          resolve()
+        })
+      )
+      res.resume()
+    })
   })
 })
 
@@ -137,38 +151,40 @@ test('encoding and mimetype', async t => {
 
   const url = await listen(fn)
 
-  form.submit(url, (err, res) => {
-    t.error(err, 'server returned a reponse')
-    t.equals(res.statusCode, 200, 'server response is 200')
-    res.pipe(
-      concat(result => {
-        const json = JSON.parse(result)
-        t.equals(
-          json.files['license'].mimetype,
-          'application/octet-stream',
-          'license mimetype'
-        )
-        t.equals(
-          json.files['readme.markdown'].mimetype,
-          'text/markdown',
-          'readme mimetype'
-        )
-        t.equals(json.files['license'].encoding, '7bit', 'license mimetype')
-        t.equals(
-          json.files['readme.markdown'].encoding,
-          '7bit',
-          'readme mimetype'
-        )
-        t.equals(
-          json.files['image.jpg'].mimetype,
-          'image/jpeg',
-          'image mimetype'
-        )
-        t.equals(json.files['image.jpg'].encoding, '7bit', 'image mimetype')
-        t.end()
-      })
-    )
-    res.resume()
+  await new Promise((resolve, reject) => {
+    form.submit(url, (err, res) => {
+      if (err) return reject(err)
+      t.equals(res.statusCode, 200, 'server response is 200')
+      res.pipe(
+        concat(result => {
+          const json = JSON.parse(result)
+          t.equals(
+            json.files['license'].mimetype,
+            'application/octet-stream',
+            'license mimetype'
+          )
+          t.equals(
+            json.files['readme.markdown'].mimetype,
+            'text/markdown',
+            'readme mimetype'
+          )
+          t.equals(json.files['license'].encoding, '7bit', 'license mimetype')
+          t.equals(
+            json.files['readme.markdown'].encoding,
+            '7bit',
+            'readme mimetype'
+          )
+          t.equals(
+            json.files['image.jpg'].mimetype,
+            'image/jpeg',
+            'image mimetype'
+          )
+          t.equals(json.files['image.jpg'].encoding, '7bit', 'image mimetype')
+          resolve()
+        })
+      )
+      res.resume()
+    })
   })
 })
 
@@ -196,21 +212,23 @@ test('custom hash with ssri', async t => {
     })
   )
 
-  form.submit(url, (err, res) => {
-    t.error(err, 'server returned a reponse')
-    t.equals(res.statusCode, 200, 'server response is 200')
-    res.pipe(
-      concat(result => {
-        const json = JSON.parse(result)
-        t.equals(
-          json.files.license.hash,
-          'sha512-gXfBuvgk+pEQjYxFnX4J+4jGSLkxxgwe0xufjULsQCjqXF26HkJV0KYXx8Uxl1Ta/dZfURsRbXlAOir+AM07Zw==',
-          'license hash is correct'
-        )
-        t.end()
-      })
-    )
-    res.resume()
+  await new Promise((resolve, reject) => {
+    form.submit(url, (err, res) => {
+      if (err) return reject(err)
+      t.equals(res.statusCode, 200, 'server response is 200')
+      res.pipe(
+        concat(result => {
+          const json = JSON.parse(result)
+          t.equals(
+            json.files.license.hash,
+            'sha512-gXfBuvgk+pEQjYxFnX4J+4jGSLkxxgwe0xufjULsQCjqXF26HkJV0KYXx8Uxl1Ta/dZfURsRbXlAOir+AM07Zw==',
+            'license hash is correct'
+          )
+          resolve()
+        })
+      )
+      res.resume()
+    })
   })
 })
 
@@ -228,40 +246,43 @@ test('custom hash with sha256', async t => {
     })
   )
 
-  form.submit(url, (err, res) => {
-    t.error(err, 'server returned a reponse')
-    t.equals(res.statusCode, 200, 'server response is 200')
-    res.pipe(
-      concat(result => {
-        const json = JSON.parse(result)
-        t.equals(
-          json.files.license.hash,
-          'a48cdb5cf55019972cab469173b77bb8adf67e6c9757e428018c4255649856f2',
-          'license hash is correct'
-        )
-        t.end()
-      })
-    )
-    res.resume()
+  await new Promise((resolve, reject) => {
+    form.submit(url, (err, res) => {
+      if (err) return reject(err)
+      t.equals(res.statusCode, 200, 'server response is 200')
+      res.pipe(
+        concat(result => {
+          const json = JSON.parse(result)
+          t.equals(
+            json.files.license.hash,
+            'a48cdb5cf55019972cab469173b77bb8adf67e6c9757e428018c4255649856f2',
+            'license hash is correct'
+          )
+          resolve()
+        })
+      )
+      res.resume()
+    })
   })
 })
 
 test('corrupt multipart', async t => {
-  t.plan(1)
   const url = await listen(fn)
 
-  const request = http.request(
-    {
-      ...urlParse(url),
-      method: 'POST',
-      headers: {
-        'content-type': 'multipart/form-data; boundar'
+  await new Promise((resolve, reject) => {
+    const request = http.request(
+      {
+        ...urlParse(url),
+        method: 'POST',
+        headers: {
+          'content-type': 'multipart/form-data; boundar'
+        }
+      },
+      res => {
+        t.equals(res.statusCode, 500, 'server response is 500')
+        resolve()
       }
-    },
-    res => {
-      res.pipe(process.stdout)
-      t.equals(res.statusCode, 500, 'server response is 500')
-    }
-  )
-  request.end()
+    )
+    request.end()
+  })
 })
